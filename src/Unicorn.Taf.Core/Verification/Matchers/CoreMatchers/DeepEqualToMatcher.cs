40d4e50f1c4b885ab93f555a7aeb192c67981aea
Vitaliy Dobriyan <dobriy88@yandex.ru>
2025-04-10 23:45:33 +0300

    added deep object comparer and corresponding matcher

---
+++ src/Unicorn.Taf.Core/Verification/Matchers/CoreMatchers/DeepEqualToMatcher.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Unicorn.Taf.Core.Utility;
+
+namespace Unicorn.Taf.Core.Verification.Matchers.CoreMatchers
+{
+    /// <summary>
+    /// Matcher to check if object is deeply equal to expected one (recursively checks all public fields and properties).
+    /// </summary>
+    /// <typeparam name="T">check items type</typeparam>
+    public class DeepEqualToMatcher<T> : TypeSafeMatcher<T>
+    {
+        private readonly T _objectToCompare;
+        private string[] pathsToIgnore;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeepEqualToMatcher{T}"/> class for specified expected object.
+        /// </summary>
+        /// <param name="objectToCompare">expected object</param>
+        public DeepEqualToMatcher(T objectToCompare)
+        {
+            _objectToCompare = objectToCompare;
+        }
+
+        public DeepEqualToMatcher<T> IgnoringPaths(params string[] pathsToIgnore)
+        {
+            this.pathsToIgnore = pathsToIgnore;
+            return this;
+        }
+            
+        /// <summary>
+        /// Gets check description.
+        /// </summary>
+        public override string CheckDescription => $"is deeply equal to '{_objectToCompare}'";
+
+        /// <summary>
+        /// Checks if object is deeply equal to expected one (recursively checks all public fields and properties).
+        /// </summary>
+        /// <param name="actual">object under check</param>
+        /// <returns>true - if actual object is deeply equal to expected one; otherwise - false</returns>
+        public override bool Matches(T actual)
+        {
+            DeepObjectsComparer comparer = new DeepObjectsComparer()
+                .UseItemsBulletsInOutput(">");
+
+            if (pathsToIgnore != null)
+            {
+                comparer.IgnorePaths(pathsToIgnore);
+            }
+
+            List<string> diff = comparer.CompareObjects(actual, _objectToCompare);
+
+            if (diff.Count == 0)
+            {
+                DescribeMismatch("objects are equal");
+                return true;
+            }
+            else
+            {
+                DescribeMismatch(string.Join(Environment.NewLine, diff));
+                return false;
+            }
+        }
+    }
+}
---
+++ src/Unicorn.UnitTests/BO/ComplexObject.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace Unicorn.UnitTests.BO
+{
+    public interface IObject
+    {
+
+    }
+
+    public class ComplexObject
+    {
+        private int privateIntField;
+
+        public ComplexObject()
+        {
+            PublicStringsListProperty = new List<string>();
+            PublicObjectsListProperty = new List<InnerObject>();
+        }
+
+        public InnerObject PublicNullField = null;
+        public InnerObject PublicInnerObject { get; set; } = new InnerObject();
+
+        public IObject PublicObjectWithInterface { get; set; }
+
+        public List<string> PublicStringsListProperty { get; set; }
+        public List<InnerObject> PublicObjectsListProperty { get; set; }
+
+        protected string ProtectedStringProperty { get; set; }
+        protected string ProtectedStringField;
+
+        public void SetPrivateInt(int value)
+        {
+            privateIntField = value;
+        }
+
+        public void SetProtectedStrings(string value)
+        {
+            ProtectedStringField = value;
+            ProtectedStringProperty = value;
+        }
+    }
+
+    public class Object1 : IObject
+    {
+    }
+
+    public class Object2 : IObject
+    {
+    }
+
+    public class InnerObject
+    {
+        public double PublicDouble { get; set; } = 0.3;
+
+        public string PublicString { get; set; } = "someValue";
+    }
+}
